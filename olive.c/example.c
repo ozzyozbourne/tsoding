d206c88c9aaadefe735dba1b82732c771a6175be
ozzyozbourne <khanosaid726@gmail.com>
2025-01-29 09:11:46 -0500

    done for the day

--- olive.c/example.c
+++ olive.c/example.c
@@ -1,5 +1,7 @@
 #include "olive.c"
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -9,22 +11,50 @@
 #define CELL_WIDTH (WIDTH/COLS)
 #define CELL_HEIGHT (HEIGHT/ROWS)
 
-#define BACK_GROUND_COLOR 0xFF202020
+#define BACKGROUND_COLOR 0xFF202020
+#define FOREGROUND_COLOR 0xFF2020FF
 
 static uint32_t pixels[WIDTH * HEIGHT];
 
-bool checkers_example(void){
-    olivec_fill(pixels, WIDTH, HEIGHT, BACK_GROUND_COLOR);
+void olivec_fill_circle(uint32_t *const pixel, const size_t pixel_width, const size_t pixel_height, 
+        const int cy, const int cx, const size_t r, const uint32_t color)
+{
+    const int r_sqrt = (int)r*r;
+    const int p_ht_cast = (int)pixel_height;
+    const int p_wd_cast = (int)pixel_width;
+
+    const int x1 = cx - (int)r;
+    const int y1 = cy - (int)r;
+    const int x2 = cx + (int)r;
+    const int y2 = cy + (int)r;
+    
+    for (int y = y1; y <= y2; y++){
+        if (0 <= y && y < p_ht_cast){
+            for (int x = x1; x <= x2; x++){
+                if (0 <= x && x < p_wd_cast){
+                   const int dx = x - cx; 
+                   const int dy = y - cy;
+                   if (dx*dx + dy*dy <= r_sqrt) { pixel[y*pixel_width + x] = color; }
+                }
+            }
+        } 
+    }
+}
+
+
+bool checkers_example(void)
+{
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
     for(int y = 0; y < ROWS; y++){
         for(int x = 0; x < COLS; x++){
-            uint32_t color = BACK_GROUND_COLOR; 
+            uint32_t color = BACKGROUND_COLOR; 
             if ((x + y)%2 == 0){ color = 0xFF0000FF; }
             olivec_fill_rect(pixels, WIDTH, HEIGHT, x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
         }
     }
 
-    const char *file_path = "output.ppm";
+    const char *file_path = "checker.ppm";
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
     if (err){
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(err));
@@ -34,8 +64,22 @@ bool checkers_example(void){
 
 }
 
+bool circle_example(void)
+{
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+    olivec_fill_circle(pixels, WIDTH, HEIGHT, WIDTH/2, HEIGHT/2, 100, FOREGROUND_COLOR);
+    const char *file_path = "circle.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err){
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(err));
+        return false;
+    }
+    return true;
+}
+
 int main(void)
 {
     if (!checkers_example()){ return -1; }
+    if (!circle_example()){ return -1; }
     return 0;
 }
--- olive.c/olive.c
+++ olive.c/olive.c
@@ -6,16 +6,16 @@
 #include <errno.h>
 #include <string.h>
 
-void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
+void olivec_fill(uint32_t *const pixels, const size_t width, const size_t height, const uint32_t color)
 {
-    for(size_t i = 0; i < width*height; i++) { pixels[i] = color; }
+    for (size_t i = 0; i < width*height; i++) { pixels[i] = color; }
 }
 
 typedef int Errno;
 
 #define return_defer(value) do { result = (value); goto defer; } while(0)
 
-Errno olivec_save_to_ppm_file(const uint32_t *const pixels, const size_t width, const size_t height, const char *file_path)
+Errno olivec_save_to_ppm_file(const uint32_t *const pixels, const size_t width, const size_t height, const char *const file_path)
 {
     int result = 0;
     FILE *f = NULL;
@@ -26,9 +26,9 @@ Errno olivec_save_to_ppm_file(const uint32_t *const pixels, const size_t width, 
         fprintf(f, "P6\n%zu %zu 255\n", width, height);
         if (ferror(f)){ return_defer(errno); }
 
-        for(size_t i = 0; i < width*height; i++){
-            uint32_t pixel = pixels[i];
-            uint8_t bytes[3] = { //Little endian so laid in memory as BGRA
+        for (size_t i = 0; i < width*height; i++){
+            const uint32_t pixel = pixels[i];
+            const uint8_t bytes[3] = { //Little endian so laid in memory as BGRA
                 (pixel>>(8*0))&0xFF, // Extract BLUE 
                 (pixel>>(8*1))&0xFF, // Extract GREEN
                 (pixel>>(8*2))&0xFF  // Extract RED 
@@ -46,14 +46,17 @@ defer:
 void olivec_fill_rect(uint32_t *const pixels, const size_t pixel_width, const size_t pixel_height, 
                       int x0, int y0, const size_t w, const size_t h, const uint32_t color)
 {
-    for(int dy = 0; dy < (int) h; dy++){
+    const int p_ht_cast = (int)pixel_height;
+    const int p_wd_cast = (int)pixel_width;
+    const int h_cast = (int)h;
+    const int w_cast = (int)w;
+
+    for (int dy = 0; dy < h_cast; dy++){
         const int y = y0 + dy; // position to start 
-        if (0 <= y && y < (int) pixel_height){
-            for(int dx = 0; dx < (int) w; dx++){
+        if (0 <= y && y < p_ht_cast){
+            for (int dx = 0; dx < w_cast; dx++){
                 const int x =  x0 + dx;
-                if(0 <= x && x < (int) pixel_width){
-                    pixels[y*pixel_width + x] = color;
-                }
+                if (0 <= x && x < p_wd_cast){ pixels[y*pixel_width + x] = color; }
             }
         }
     }       
