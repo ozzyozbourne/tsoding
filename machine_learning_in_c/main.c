4687d68a3c955b8fd5bc68c34a24de6c0b5230b9
ozzyozbourne <khanosaid726@gmail.com>
2025-05-24 15:10:29 -0400

    adws

--- machine_learning_in_c/main.c
+++ machine_learning_in_c/main.c
@@ -16,17 +16,25 @@ float rand_float(void){
     return (float) rand()/ (float)RAND_MAX;
 }
 
+float cost(float w) {
+    float result = 0.0f;
+    for(size_t i = 0; i < train_count; i++){
+        float x = train[i][0];
+        float y = x*w;
+        float d = y - train[i][1];
+        result += d*d;
+    }
+    result /= train_count;
+    return result;
+}
+
 int main(void)
 {
     // srand(time(NULL));
     srand(69);
     // y = x*w;
     float w = rand_float()*10.0f;
-    printf("Weight is -> %f\n", w);
-    for(size_t i = 0; i < train_count; i++){
-        float x = train[i][0];
-        float y = x*w;
-        printf("Actual: %f, Expected: %f\n", y, train[i][1]);
-    }
+    printf("%f\n", cost(w));
+
     return 0;
 }
--- machine_learning_in_odin/main.odin
+++ machine_learning_in_odin/main.odin
@@ -3,16 +3,21 @@ package main
 import "core:fmt"
 import "core:math/rand"
 
-train := [][2]f32{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+train := [][2]f64{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
 
-
-main :: proc() {
-	// y = x * w
-	w := rand.float32()
-	fmt.printfln("The Weight is -> %f", w)
+cost :: proc(w: f64) -> (res: f64) {
 	for v, i in train {
 		x := v[0]
 		y := x * w
-		fmt.printfln("Actual: %f   Expected: %f", y, v[1])
+		d := y - v[1]
+		res += (d * d)
 	}
+	res /= f64(len(train))
+	return
+}
+
+main :: proc() {
+	// y = x * w
+	w := rand.float64() * 10
+	fmt.println(cost(w))
 }
